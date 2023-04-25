530770097f702f664c28dae61bc43f0cb6d37763
projeto13aplicatiovs <fabiocorrena@gmail.com>
2023-04-25 00:06:38 -0300

    https://github.com/Binho84/novoprojeto3.0.git

---
+++ udemy13aplicativos/13aplicativos/src/main/java/devandroide/fabio/a13aplicativos/MainActivity.java
@@ -0,0 +1,13 @@
+package devandroide.fabio.a13aplicativos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
